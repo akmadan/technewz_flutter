e4455fb6c610f37f135c81c70e974dcdfcc17441
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2021-06-28 18:15:34 +0530

    location1

--- lib/backend/functions.dart
+++ lib/backend/functions.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:technewz/components/location.dart';
 import 'package:technewz/components/searchbar.dart';
 import 'package:technewz/utils/key.dart';
 
 Future<List> fetchnews() async {
   final response = await http.get(
-    Uri.parse(
-        'https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=100&apiKey=' +
-            Key.key +
-            '&q=' +
-            SearchBar.searchcontroller.text),
+    Uri.parse('https://newsapi.org/v2/top-headlines?country=' +
+        Location.location +
+        '&category=technology&pageSize=100&apiKey=' +
+        Key.key +
+        '&q=' +
+        SearchBar.searchcontroller.text),
   );
   Map result = jsonDecode(response.body);
   print(result['articles']);
--- lib/components/location.dart
+++ lib/components/location.dart
@@ -1,3 +1,49 @@
 import 'package:flutter/material.dart';
 
+class Location extends StatefulWidget {
+  static String location = 'us';
+  const Location({Key? key}) : super(key: key);
 
+  @override
+  _LocationState createState() => _LocationState();
+}
+
+class _LocationState extends State<Location> {
+  List<String> locationcodes = ['us', 'in', 'de', 'jp', 'gb', 'kr', 'cn'];
+  String dropdownValue = 'USA';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: DropdownButton<String>(
+        value: dropdownValue,
+        icon: const Icon(Icons.arrow_downward),
+        iconSize: 24,
+        elevation: 16,
+        style: const TextStyle(color: Colors.deepPurple),
+        underline: Container(
+          height: 2,
+          color: Colors.deepPurpleAccent,
+        ),
+        onChanged: (String? newValue) {
+          setState(() {
+            dropdownValue = newValue!;
+          });
+        },
+        items: <String>[
+          'USA',
+          'India',
+          'Germany',
+          'Japan',
+          'UK',
+          'South Korea',
+          'China'
+        ].map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
